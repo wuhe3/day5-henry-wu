fdc3a34a80134e995a0ad484b507f2396b24164f
WUHE3 <wuhe3@oocl.com>
2024-11-22 11:48:45 +0800

    [ParkingLot][Story1][test] add should_return_a_ticket_when_park_given_a_car()

---
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -0,0 +1,12 @@
+package com.parkinglot;
+import java.util.List;
+
+
+public class ParkingLot {
+    private int vacancy;
+    private List<Car> cars;
+
+    public Ticket park(Car car) {
+        return new Ticket();
+    }
+}
