b819eacfec0f9c76acc9099ebe0cccd70d39549f
wuhe3 <wuhe3@oocl.com>
2024-11-24 04:28:00 +0800

    [ParkingLot][Story5][Refactor] refactor park, using Stream()

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -1,19 +1,13 @@
 package com.parkinglot;
 
+import java.util.Comparator;
+
 public class SmartParkingBoy extends StandardParkingBoy {
     @Override
     public Ticket park(Car car) {
-        ParkingLot parkingLotWithMoreEmptyPositions = parkingLots.get(0);
-        for (ParkingLot parkingLot : parkingLots) {
-            if (parkingLot.getVaccancy() > parkingLotWithMoreEmptyPositions.getVaccancy()) {
-                parkingLotWithMoreEmptyPositions = parkingLot;
-            }
-        }
-        if (parkingLotWithMoreEmptyPositions.isFull()) {
-            throw new NoAvailablePositionException();
-        } else {
-            return parkingLotWithMoreEmptyPositions.park(car);
-        }
-
+        ParkingLot parkingLotWithMoreEmptyPositions = parkingLots.stream()
+                .max(Comparator.comparing(ParkingLot::getVaccancy))
+                .orElseThrow(NoAvailablePositionException::new);
+        return parkingLotWithMoreEmptyPositions.park(car);
     }
 }
